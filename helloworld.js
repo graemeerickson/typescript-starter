3e1680d61de45e9c2729d15e66373a66fc955c78
graemeerickson <graemeerickson@gmail.com>
2019-01-18 15:10:13 -0800

    Testing the 'any' type

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,4 @@
 let num = 100;
 let myStr = 'Hello world!';
-const myArr = [1, 2, 3, 'test string'];
+const myArr = [1, 2, 3];
+const anyArr = [1, 2, 3, 'test string', true];
--- helloworld.ts
+++ helloworld.ts
@@ -1,3 +1,4 @@
 let num:number = 100;
 let myStr:string = 'Hello world!';
-const myArr:number[] = [1, 2, 3, 'test string'];
+const myArr:number[] = [1, 2, 3];
+const anyArr:any[] = [1, 2, 3, 'test string', true];
