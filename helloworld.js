f65ba4c69c41a23b48494133d03c873f0564592d
graemeerickson <graemeerickson@gmail.com>
2019-01-18 15:19:41 -0800

    Implemented enum data type

--- helloworld.js
+++ helloworld.js
@@ -2,3 +2,13 @@ let num = 100;
 let myStr = 'Hello world!';
 const myArr = [1, 2, 3];
 const anyArr = [1, 2, 3, 'test string', true];
+var myConstantsEnum;
+(function (myConstantsEnum) {
+    myConstantsEnum[myConstantsEnum["pi"] = 3.14] = "pi";
+    myConstantsEnum[myConstantsEnum["e"] = 2.73] = "e";
+    myConstantsEnum[myConstantsEnum["log2"] = 0.3] = "log2";
+    myConstantsEnum[myConstantsEnum["log5"] = 0.7] = "log5";
+})(myConstantsEnum || (myConstantsEnum = {}));
+const radius = 10;
+console.log('Finding circumference of circle');
+console.log(2 * myConstantsEnum.pi * radius);
--- helloworld.ts
+++ helloworld.ts
@@ -2,3 +2,12 @@ let num:number = 100;
 let myStr:string = 'Hello world!';
 const myArr:number[] = [1, 2, 3];
 const anyArr:any[] = [1, 2, 3, 'test string', true];
+enum myConstantsEnum {
+  pi = 3.14,
+  e = 2.73,
+  log2 = 0.3,
+  log5 = 0.7
+}
+const radius:number = 10;
+console.log('Finding circumference of circle');
+console.log(2 * myConstantsEnum.pi * radius);
